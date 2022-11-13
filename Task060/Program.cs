d91d1c9d2a0b4b841d812236bc80ba6ec1652aa8
Valeria <v.rumyantceva@gmail.com>
2022-11-14 00:20:21 +0300

    add Task060

---
+++ Task060/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 60. 
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
+{
+    var matrix = new int[rows, columns, depth];
+    var rnd = new Random();
+    bool Unique = false;
+    for (int n = 0; n < matrix.GetLength(0); n++)
+    {
+         Unique = false;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Unique = false;
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Unique = false;
+                int temp = rnd.Next(min, max + 1);
+                for (int u = 0; u < 27; u++)
+                {
+                    if (matrix[n, j, k] == temp)
+                    { 
+                        Unique = true;
+                        break;
+                    }
+                }
+                if (!Unique) 
+                matrix[n, j, k] = temp;
+
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("|");
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (k < matrix.GetLength(2) - 1) Console.Write($"{matrix[i, j, k],4}|");
+                else Console.Write($"{matrix[i, j, k],4}");
+            }
+            Console.WriteLine("|");
+        }
+        Console.WriteLine("");
+    }
+}
+
+
+
+int[,,] array3D = CreateMatrixRndInt(3, 3, 3, 0, 30);
+PrintMatrix(array3D);
