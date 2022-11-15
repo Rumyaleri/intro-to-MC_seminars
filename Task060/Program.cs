9a1c52dd05c15ef47e29e88769f3193aae3a87ec
Valeria <v.rumyantceva@gmail.com>
2022-11-15 22:14:54 +0300

    add Task060

--- Task060/Program.cs
+++ Task060/Program.cs
@@ -6,36 +6,70 @@
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
-int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
-{
-    var matrix = new int[rows, columns, depth];
-    var rnd = new Random();
-    bool Unique = false;
-    for (int n = 0; n < matrix.GetLength(0); n++)
-    {
-         Unique = false;
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-             Unique = false;
-            for (int k = 0; k < matrix.GetLength(2); k++)
-            {
-                Unique = false;
-                int temp = rnd.Next(min, max + 1);
-                for (int u = 0; u < 27; u++)
-                {
-                    if (matrix[n, j, k] == temp)
-                    { 
-                        Unique = true;
-                        break;
-                    }
-                }
-                if (!Unique) 
-                matrix[n, j, k] = temp;
+Console.WriteLine("Введите количество строк матрицы: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбиков матрицы: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество слоев матрицы: ");
+int z = Convert.ToInt32(Console.ReadLine());
 
+int[] CreateUniqueElem(int x, int y,int z, int min, int max)
+{
+    int[] arr = new int[x*y*z];
+    var rnd = new Random();
+    int temp = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+        temp = arr[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (arr[i] == arr[j])
+                {
+                    arr[i] = rnd.Next(min, max + 1);
+                    j = 0;
+                    temp = arr[i];
+                }
+                temp = arr[i];
+            }
+        }
+
+    }
+    // сортировка = проверка элементов массива на уникальность
+    // for (int i = 0; i < arr.Length; i++)
+    //         {
+    //             for (int j = 0; j < i; j++)
+    //             {
+    //             if (arr[i] < arr[j])
+    //             {
+    //                 var t = arr[i];
+    //                 arr[i] = arr[j];
+    //                 arr[j] = t;
+    //             }
+    //         }
+    //         }
+    return arr;
+}
+
+int[,,] CreateMatrix3DInt(int x, int y,int z, int [] arr)
+{
+
+    int [,,] matr3D = new int [x, y, z];
+    int a = 0;
+    for (int i = 0; i < matr3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr3D.GetLength(2); k++)
+            {
+                matr3D[i, j, k] = arr[a];
+                a++;
             }
         }
     }
-    return matrix;
+    return matr3D;
 }
 
 void PrintMatrix(int[,,] matrix)
@@ -44,19 +78,30 @@ void PrintMatrix(int[,,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write("|");
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
-                if (k < matrix.GetLength(2) - 1) Console.Write($"{matrix[i, j, k],4}|");
-                else Console.Write($"{matrix[i, j, k],4}");
+                if (k < matrix.GetLength(2) - 1) Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+                else Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
             }
-            Console.WriteLine("|");
         }
         Console.WriteLine("");
     }
 }
 
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+//         else Console.Write($"{array[i]}");
+//     }
+//     Console.WriteLine("]");
+// }
 
 
-int[,,] array3D = CreateMatrixRndInt(3, 3, 3, 0, 30);
+
+int[] list = CreateUniqueElem(x, y,z, 10, 50);
+// PrintArray(list);
+int[,,] array3D = CreateMatrix3DInt(x, y, z, list);
 PrintMatrix(array3D);
