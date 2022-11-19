c3f97cd7ad2c1ca5c12d5781263fbea40b667af4
Valeria <v.rumyantceva@gmail.com>
2022-11-19 22:04:02 +0300

    add Task068

--- Task068/Program.cs
+++ Task068/Program.cs
@@ -3,3 +3,25 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите целое число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0 || n < 0)
+{
+    Console.WriteLine("Ведены некорректные данные");
+    return;
+}
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return AkkermanFunction(m - 1, 1);
+    else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+int a = AkkermanFunction(m, n);
+Console.Write($"Функция Аккермана = {a} ");
